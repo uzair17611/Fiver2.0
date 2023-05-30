6f03832c2244bc6a6609bb8bc82319afeb67808b
uzair1761 <ozairkhanan@gmail.com>
2023-05-31 02:24:39 +0500

    login and logout done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,15 +3,27 @@ import "./Navbar.scss"
 import { useState } from 'react';
 import {Link} from "react-router-dom"
 import { useLocation } from 'react-router-dom';
-// import noavatar   from "../../public/noav"
-
+import noavatar from  "../../public/img/noavatar.jpg"
+import newRequest from '../../Utilis/newRequest';
+import { useNavigate } from 'react-router-dom';
 const Navbar = () => {
     const [Active ,SetActive] = useState(false);
     const [open ,Setopen] = useState(false);
 
-
+const navigate =useNavigate()
     const isActive =()=>{
         window.scrollY > 0 ?  SetActive( true) : SetActive(false)
+
+    }
+
+    const HandleSubmit =  async()=>{
+          try {
+         await   newRequest.post("/auth/logout")
+         localStorage.setItem("currentUser" ,null)
+         navigate("/")
+          } catch (error) {
+            console.log(err)
+          }
 
     }
 
@@ -45,8 +57,8 @@ const Navbar = () => {
         {currentuser && (
             <>
             <div className="user"  onClick={()=>Setopen(!open)}>
-                <img  src={currentuser.imggot } alt='image'></img>
-                <span>{currentuser.userName}</span>
+                <img  src={currentuser.img || noavatar} alt='image'></img>
+                <span>{currentuser.username}</span>
                 {open && <div className="options">
                 {currentuser ?.isSeller  && (
                    <>
@@ -57,7 +69,9 @@ const Navbar = () => {
                 )}
                 <Link to="/order" className='link'>order</Link>
                 <Link to="/massages" className='link'>Messeges</Link>
-                <Link to="/" className='link'>Logout</Link>
+                <Link to="/" className='link'  onClick={HandleSubmit}>
+                    
+                    Logout</Link>
                 </div>}
             </div>
             </>
